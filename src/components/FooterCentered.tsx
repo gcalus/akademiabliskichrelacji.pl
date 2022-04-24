bcb2b5842d49fe80ab74cb1c6a46d77e4dce0b2a
Marek Całus <marek.calus3@gmail.com>
2022-04-24 22:31:42 +0200

    Add links to the main site

--- src/components/FooterCentered.tsx
+++ src/components/FooterCentered.tsx
@@ -19,7 +19,7 @@ const FooterCentered = (props: ICenteredFooterProps) => (
       </ul>
     </nav>
 
-    <div className="mt-8 flex justify-center">
+    <div className="flex justify-center">
       <FooterIconList>{props.iconList}</FooterIconList>
     </div>
 
