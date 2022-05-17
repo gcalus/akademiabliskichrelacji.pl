f0e188a81b64dae38bacfc299086927bf47f9db0
Marek Całus <marek.calus3@gmail.com>
2022-05-17 22:27:13 +0200

    .

--- src/components/FundamentButton.tsx
+++ src/components/FundamentButton.tsx
@@ -1,16 +1,12 @@
-import Link from 'next/link';
-
 import { Button } from './Button';
 
 const FundamentButton = () => {
   return (
-    <Link href="https://sklep.gosiacalus.pl/produkt/program-mentoringowy-zbuduj-solidny-fundament-relacji/">
-      <a>
-        <Button xl variant="gradient">
-          Dołącz do programu
-        </Button>
-      </a>
-    </Link>
+    <a href="https://sklep.gosiacalus.pl/produkt/program-mentoringowy-zbuduj-solidny-fundament-relacji/">
+      <Button xl variant="gradient">
+        Dołącz do programu
+      </Button>
+    </a>
   );
 };
 
