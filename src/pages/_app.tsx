080218b9cbc0580d97094420ef1c6248b2b7fe5b
Marek Całus <marek.calus3@gmail.com>
2022-11-16 13:41:17 +0100

    Add fpixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,52 @@
-import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
-import FacebookPixel from "../components/FacebookPixel";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Script from "next/script";
 
 import "../utils/global.css";
+import * as fbq from "../utils/fpixel";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
-    <FacebookPixel />
-    <Component {...pageProps} />
-  </>
-);
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview();
+
+    const handleRouteChange = () => {
+      fbq.pageview();
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Global Site Code Pixel - Facebook Pixel */}
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,36 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 import { AppConfig } from "../utils/AppConfig";
+import { FB_PIXEL_ID } from "../utils/fpixel";
 
-// Need to create a custom _document because i18n support is not compatible with `next export`.
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang={AppConfig.locale}>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
+export default function DocumentPage() {
+  return (
+    <Html lang={AppConfig.locale}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
           />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800&display=swap"
-            rel="stylesheet"
-          />
-          <meta
-            name="facebook-domain-verification"
-            content="chztizczwivhu48bqcgmzgfnrxldtu"
-          />
-        </Head>
-        <body className="antialiased text-slate-700">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        </noscript>
+      </Head>
+      <body className="antialiased text-slate-700">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
